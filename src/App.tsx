b1fb317954bf51e35dddb43b545c8c2c9c2cfb92
ruth <hofferruth14@gmail.com>
2024-04-27 21:38:10 -0300

    mi proyecto subido

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Items, Pokemon, Pokemons } from './pages';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path="/pokemons/:name" element={<Pokemon />} />
+        <Route path="/items" element={<Items/>}/>
+        <Route path="/pokemon" element={<Pokemon/>}/>
+        <Route path="/" element={<Pokemons />}/>
+      </Routes>
+    </BrowserRouter>
+  )
 }
+
 
 export default App;
---
+++ src/pages/index.tsx
@@ -0,0 +1,3 @@
+export {default as Items} from './items';
+export {default as Pokemon} from './pokemon';
+export {default as Pokemons} from './pokemons';
